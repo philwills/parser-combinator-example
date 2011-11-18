91c2b30e40a01b6544bfd432fda836f14a772bdd
Philip Wills <philip.wills@guardian.co.uk>
2011-11-18 13:55:28 +0000

    Simplest case of matching a pattern and producing some output

---
+++ src/main/scala/PathPatternParser.scala
@@ -0,0 +1,15 @@
+package com.gu
+
+import util.parsing.combinator.RegexParsers
+
+object PathPatternParser extends RegexParsers {
+  def pathPattern = """.*""".r ^^ { path => path }
+
+  def parseFrom(path: String) = {
+    val tokens = parseAll(pathPattern, path) match {
+      case Success(tokens, _) => tokens
+      case _ => throw new IllegalArgumentException("Invalid path pattern: " + path)
+    }
+    (tokens.r, List())
+  }
+}
---
+++ src/test/scala/PathPatternSpec.scala
@@ -0,0 +1,12 @@
+package com.gu
+
+import org.specs2.mutable._
+
+class PathPatternSpec extends Specification {
+  "A literal path" should {
+    "produce a regex matching a literal path" in {
+      val (pattern, _) = PathPatternParser.parseFrom("/simple/path")
+      pattern.toString() must be ("/simple/path")
+    }
+  }
+}

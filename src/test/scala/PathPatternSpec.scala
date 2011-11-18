5c4066d089f7969336ee7d82139c486a74a7d736
Philip Wills <philip.wills@guardian.co.uk>
2011-11-18 14:10:51 +0000

    Less in test through imports

--- src/test/scala/PathPatternSpec.scala
+++ src/test/scala/PathPatternSpec.scala
@@ -1,11 +1,12 @@
 package com.gu
 
 import org.specs2.mutable._
+import PathPatternParser._
 
 class PathPatternSpec extends Specification {
   "A literal path" should {
     "produce a regex matching a literal path" in {
-      val pattern = PathPatternParser.parseFrom("/simple/path")
+      val pattern = parseFrom("/simple/path")
       pattern.toString must be ("/simple/path")
     }
   }

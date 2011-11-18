986b4d34a6e3672c87cbe360da166fff6ac73454
Philip Wills <philip.wills@guardian.co.uk>
2011-11-18 14:05:03 +0000

    Less braces

--- src/test/scala/PathPatternSpec.scala
+++ src/test/scala/PathPatternSpec.scala
@@ -6,7 +6,7 @@ class PathPatternSpec extends Specification {
   "A literal path" should {
     "produce a regex matching a literal path" in {
       val pattern = PathPatternParser.parseFrom("/simple/path")
-      pattern.toString() must be ("/simple/path")
+      pattern.toString must be ("/simple/path")
     }
   }
 }

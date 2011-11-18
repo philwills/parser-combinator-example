02e64f97310a171824f26788db5e6a2c0a1b6cfe
Philip Wills <philip.wills@guardian.co.uk>
2011-11-18 15:08:28 +0000

    Working for splats anywhere in the path

--- src/main/scala/PathPatternParser.scala
+++ src/main/scala/PathPatternParser.scala
@@ -3,18 +3,18 @@ package com.gu
 import util.parsing.combinator.RegexParsers
 
 object PathPatternParser extends RegexParsers {
-  def pathPattern = """[^*]*""".r ~ opt("*") ^^ {
-    case path ~ splat => splat match  {
-      case Some(_) => path + """.*?"""
-      case None => path
-    }
-  }
+  def splat = "*" ^^^ { """.*?"""}
+  def simpleDirectory = """[^/]*""".r ^^ { identity }
+
+  def directory = "/" ~> (splat | simpleDirectory)
+
+  def pathPattern = rep(directory)
 
   def parseFrom(path: String) = {
     val tokens = parseAll(pathPattern, path) match {
       case Success(tokens, _) => tokens
       case _ => throw new IllegalArgumentException("Invalid path pattern: " + path)
     }
-    tokens.r
+    tokens.fold("")( _ + "/" + _).r
   }
 }
--- src/test/scala/PathPatternSpec.scala
+++ src/test/scala/PathPatternSpec.scala
@@ -7,14 +7,18 @@ class PathPatternSpec extends Specification {
   "A literal path" should {
     "produce a regex matching a literal path" in {
       val pattern = parseFrom("/simple/path")
-      pattern.toString must be ("/simple/path")
+      pattern.toString must be equalTo ("/simple/path")
     }
   }
 
   "A splat in a path" should {
-    "match  anything" in {
+    "match  anything at the end" in {
       val pattern = parseFrom("/splat/path/*")
-      pattern.toString must be ("/splat/path/.*?")
+      pattern.toString must be equalTo ("/splat/path/.*?")
+    }
+    "match anything in the middle" in {
+      val pattern = parseFrom("/splat/*/path")
+      pattern.toString must be equalTo ("/splat/.*?/path")
     }
   }
 }
